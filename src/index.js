0fa69e1c10eae8ca853de913dd1f3dabe462ba55
kate <kan004@ucr.edu>
2021-10-18 18:13:26 -0700

    css import

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import foods from "./foods";
-import { choice, remove } from "./helpers";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-let fruit = choice(foods);
+ReactDOM.render(<App />, document.getElementById("root"));
 
-console.log(`I'd like one ${fruit} please`);
-console.log(`Here you go: random ${fruit}`);
-console.log("Delicious! May I have another?");
-let remaining = remove(foods, fruit);
-console.log(
-  `I'm sorry, we're all out. We have ${remaining.length} other fruits left`
-);
+// export default App;
+// random fruits
+// import foods from "./foods";
+// import { choice, remove } from "./helpers";
+
+// let fruit = choice(foods);
+
+// console.log(`I'd like one ${fruit} please`);
+// console.log(`Here you go: random ${fruit}`);
+// console.log("Delicious! May I have another?");
+// let remaining = remove(foods, fruit);
+// console.log(
+//   `I'm sorry, we're all out. We have ${remaining.length} other fruits left`
+// );
